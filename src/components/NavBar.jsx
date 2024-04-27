ff5fe70c388b9e8e5a71dafdbd2b6bd3b98fd4c1
teodorSmilevski <teosmilevski@outlook.com>
2024-04-27 23:50:19 +0200

    NavBar finished

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,37 @@
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 import logo from "../assets/logo.png";
+import { useState } from "react";
 export default function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function handleMenuClick() {
+    setIsMenuOpen((oldState) => !oldState);
+  }
+  function turnToFalse() {
+    setIsMenuOpen(false);
+  }
   return (
     <nav>
       <Link to="/" className="logo">
         <img src={logo} alt="logo" />
       </Link>
-      <label htmlFor="" className="menu-icon">
-        <i className="bx bx-menu"></i>
+      <label htmlFor="" className="menu-icon" onClick={handleMenuClick}>
+        {!isMenuOpen ? <i className="bx bx-menu"></i> : <p>✖</p>}
       </label>
       <div>
-        <ul>
-          <li>
+        <ul className={isMenuOpen ? "dropdown-menu" : "menu-list"}>
+          <li onClick={turnToFalse}>
             <Link to="/">Home</Link>
           </li>
 
-          <li>
+          <li onClick={turnToFalse}>
             <Link to="/features">Features</Link>
           </li>
-          <li>
+          <li onClick={turnToFalse}>
             <Link to="/workouts">Workouts</Link>
           </li>
-          <li>
+          <li onClick={turnToFalse}>
             <Link to="/about">About</Link>
           </li>
         </ul>
